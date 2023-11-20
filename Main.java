ae3655a01cabb3fd7e11f75d0f130246ef8fc21e
ayoub ezzaaraoui <146847154+ezzaaraoui@users.noreply.github.com>
2023-11-20 12:05:05 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main{
     public static void main(String[] args) {
-        Car car = new Car();
+        Car car = new Car(200 , "red" ,true , "BMW " , 120 , "veen");
         car.color = "red" ;
         car.model = "BMW" ;
         car.speed = 280 ;
